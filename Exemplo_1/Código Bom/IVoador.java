e4f4650c7212d1c22cc3927dae07299c12ac68d2
dawnnn <_tiago_@live.com>
2015-09-24 15:58:18 -0300

    Arquivos POO 2

---
+++ Exemplo_1/Código Bom/IVoador.java
@@ -0,0 +1,4 @@
+public interface IVoador
+{
+   void Voar();
+}
---
+++ Exemplo_1/Código Ruim/IAnimal.java
@@ -0,0 +1,12 @@
+public interface IAnimal
+{
+   string Nome { get; set; }
+ 
+   string Reino { get; set; }
+   
+   void Correr();
+ 
+   void Voar();
+ 
+   void Nadar();
+}
